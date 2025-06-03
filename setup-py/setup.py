00afb208c5d0a37e3abdbdcbe99d63a7c524a7ae
Michael Booth <michael.booth@skyscanner.net>
2025-06-03 14:50:39 +0100

    test python updates

---
+++ setup-py/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+install_requires = [
+    'requests==2.28.1',
+]
+
+dev_requires = [
+    'pytest==7.2.2',
+]
+
+
+setup(
+    name="tester",
+    version="0.0.1",
+    description="Test project",
+    include_package_data=True,
+    platforms="any",
+    install_requires=install_requires,
+    extras_require={
+        'dev': dev_requires,
+    }
+)
